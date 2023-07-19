9bef2833a44e721dd138c9f56711efbfcdbe7adf
Sherry <yangxuebg@outlook.com>
2023-07-19 10:38:36 -0400

    Add link to the card

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -12,25 +12,29 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card
-      _hover={{
-        transform: "scale(1.03)",
-      }}
-    >
-      <Image src={getCroppedImageUrl(game.background_image)} />
-      <CardBody>
-        <HStack justifyContent="space-between" marginBottom={3}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-        <Heading fontSize="2xl">
-          <Link to={"/games/" + game.slug}>{game.name}</Link>
-          <Emoji rating={game.rating_top} />
-        </Heading>
-      </CardBody>
-    </Card>
+    <Link to={"/games/" + game.slug}>
+      <Card
+        _hover={{
+          transform: "scale(1.03)",
+        }}
+      >
+        <Image src={getCroppedImageUrl(game.background_image)} />
+
+        <CardBody>
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+
+          <Heading fontSize="2xl">
+            <Link to={"/games/" + game.slug}>{game.name}</Link>
+            <Emoji rating={game.rating_top} />
+          </Heading>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
